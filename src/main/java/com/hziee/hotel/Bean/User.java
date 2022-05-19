6fc78ed1d39be0389e35d68dee8c5edea6a9a45a
SakiLazy <3149207528@qq.com>
2022-05-19 12:34:19 +0800

    增加user

---
+++ src/main/java/com/hziee/hotel/Bean/User.java
@@ -0,0 +1,71 @@
+package com.hziee.hotel.Bean;
+
+public class User {
+    private int id;
+    private String name;
+    private String password;
+    private String phone;
+    private String mail;
+
+    public User(){
+
+    }
+    public User(int id,String name,String password,String phone,String mail) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+        this.mail = mail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+}
