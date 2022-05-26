9228875597e056ea94b618f3538a2abe925cb428
SakiLazy <3149207528@qq.com>
2022-05-26 13:20:58 +0800

    other

---
+++ src/main/java/com/hziee/hotel/Bean/Order.java
@@ -0,0 +1,88 @@
+package com.hziee.hotel.Bean;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Order {
+	private int id;
+	private String user_name;
+	private String type;
+	private String price;
+	private Date in_date;
+	private Date out_date;
+
+	public Order() {
+
+	}
+
+	public Order(int id, String user_name, String type, String price, Date in_date, Date out_date) {
+		this.id = id;
+		this.user_name = user_name;
+		this.type = type;
+		this.price = price;
+		this.in_date = in_date;
+		this.out_date = out_date;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public Date getIn_date() {
+		return in_date;
+	}
+
+	public void setIn_date(Date in_date) {
+		this.in_date = in_date;
+	}
+
+	public Date getOut_date() {
+		return out_date;
+	}
+
+	public void setOut_date(Date out_date) {
+		this.out_date = out_date;
+	}
+
+	@Override
+	public String toString() {
+		return "Order{" +
+				"id=" + id +
+				", user_name='" + user_name + '\'' +
+				", type='" + type + '\'' +
+				", price='" + price + '\'' +
+				", in_date=" + in_date +
+				", out_date=" + out_date +
+				'}';
+	}
+}
--- src/main/java/com/hziee/hotel/Bean/Room.java
+++ src/main/java/com/hziee/hotel/Bean/Room.java
@@ -8,5 +8,43 @@ public class Room {
 	private String price;
 	private String stock;
 
+	public Room(String type, String price, String stock) {
+		this.type = type;
+		this.price = price;
+		this.stock = stock;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getStock() {
+		return stock;
+	}
+
+	public void setStock(String stock) {
+		this.stock = stock;
+	}
+
+	@Override
+	public String toString() {
+		return "Room{" +
+				"type='" + type + '\'' +
+				", price='" + price + '\'' +
+				", stock='" + stock + '\'' +
+				'}';
+	}
 
 }
