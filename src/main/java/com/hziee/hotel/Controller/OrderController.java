fa15817ae9b73bf648d3890e06d26d86bca8ea4a
SakiLazy <3149207528@qq.com>
2022-05-26 13:28:23 +0800

    other

--- src/main/java/com/hziee/hotel/Controller/OrderController.java
+++ src/main/java/com/hziee/hotel/Controller/OrderController.java
@@ -1,8 +1,14 @@
 package com.hziee.hotel.Controller;
 
+import com.hziee.hotel.Bean.Order;
+import com.hziee.hotel.Service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class OrderController {
-    
+
+    @Autowired
+    OrderService orderService;
+
 }
