28061e7b0645b018dc9efa2d9f9e462960f3cbe9
SakiLazy <3149207528@qq.com>
2022-05-26 08:52:50 +0800

    other

--- src/main/java/com/hziee/hotel/Controller/AdminController.java
+++ src/main/java/com/hziee/hotel/Controller/AdminController.java
@@ -1,10 +1,6 @@
 package com.hziee.hotel.Controller;
 
-import com.hziee.hotel.Bean.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AdminController {
